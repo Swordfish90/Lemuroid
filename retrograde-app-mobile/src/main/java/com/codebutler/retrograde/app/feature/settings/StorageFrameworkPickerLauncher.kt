f06ebd6cf8c4cf5108a2ec831ce7fda84716e6ec
Filippo Scognamiglio <flscogna@gmail.com>
2019-10-04 23:43:50 +0200

    Take persistent uris when using storage access framework.

--- retrograde-app-mobile/src/main/java/com/codebutler/retrograde/app/feature/settings/StorageFrameworkPickerLauncher.kt
+++ retrograde-app-mobile/src/main/java/com/codebutler/retrograde/app/feature/settings/StorageFrameworkPickerLauncher.kt
@@ -14,6 +14,9 @@ class StorageFrameworkPickerLauncher : FragmentActivity() {
         super.onCreate(savedInstanceState)
         if (savedInstanceState == null) {
             val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE).apply {
+                this.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                this.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION)
+                this.addFlags(Intent.FLAG_GRANT_PREFIX_URI_PERMISSION)
             }
             startActivityForResult(intent, REQUEST_CODE_PICK_FOLDER)
         }
@@ -23,13 +26,30 @@ class StorageFrameworkPickerLauncher : FragmentActivity() {
         super.onActivityResult(requestCode, resultCode, resultData)
 
         if (requestCode == REQUEST_CODE_PICK_FOLDER && resultCode == Activity.RESULT_OK) {
+            val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
             val preferenceKey = getString(R.string.pref_key_extenral_folder)
-            PreferenceManager.getDefaultSharedPreferences(this).edit().apply {
-                this.putString(preferenceKey, resultData?.data.toString())
+
+            clearPreviousPersistentUris()
+
+            val newRootUri = resultData?.data
+            newRootUri?.let {
+                contentResolver.takePersistableUriPermission(it, Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            }
+
+            sharedPreferences.edit().apply {
+                this.putString(preferenceKey, newRootUri.toString())
                 this.apply()
             }
         }
         finish()
+    }
+
+    private fun clearPreviousPersistentUris() {
+        contentResolver.persistedUriPermissions
+            .filter { it.isReadPermission }
+            .forEach {
+                contentResolver.releasePersistableUriPermission(it.uri, Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            }
     }
 
     companion object {
