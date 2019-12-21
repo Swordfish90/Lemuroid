67dcda368e011ff34a0f5a1049fb0fa9c5abc73e
Filippo Scognamiglio <flscogna@gmail.com>
2019-12-21 13:21:55 +0100

    Bump versions.

--- lemuroid-app/build.gradle.kts
+++ lemuroid-app/build.gradle.kts
@@ -12,8 +12,8 @@ plugins {
 android {
     defaultConfig {
         applicationId = "com.swordfish.lemuroid"
-        versionCode = 4
-        versionName = "0.0.2"
+        versionCode = 6
+        versionName = "0.0.3"
     }
 
     signingConfigs {
