5c40852a678420ef3defcaefcfa679061842b1ec
Filippo Scognamiglio <flscogna@gmail.com>
2020-01-04 16:21:38 +0100

    Change strings in contextual menu.

--- lemuroid-app/src/main/java/com/swordfish/lemuroid/app/shared/GameContextMenuListener.kt
+++ lemuroid-app/src/main/java/com/swordfish/lemuroid/app/shared/GameContextMenuListener.kt
@@ -11,23 +11,23 @@ class GameContextMenuListener(
 ) : View.OnCreateContextMenuListener {
 
     override fun onCreateContextMenu(menu: ContextMenu, v: View, menuInfo: ContextMenu.ContextMenuInfo?) {
-        menu.add(R.string.play).setOnMenuItemClickListener {
+        menu.add(R.string.game_context_menu_resume).setOnMenuItemClickListener {
             gameInteractor.onGamePlay(game)
             true
         }
 
-        menu.add(R.string.restart).setOnMenuItemClickListener {
+        menu.add(R.string.game_context_menu_restart).setOnMenuItemClickListener {
             gameInteractor.onGameRestart(game)
             true
         }
 
         if (game.isFavorite) {
-            menu.add(R.string.remove_from_favorites).setOnMenuItemClickListener {
+            menu.add(R.string.game_context_menu_remove_from_favorites).setOnMenuItemClickListener {
                 gameInteractor.onFavoriteToggle(game, false)
                 true
             }
         } else {
-            menu.add(R.string.add_to_favorites).setOnMenuItemClickListener {
+            menu.add(R.string.game_context_menu_add_to_favorites).setOnMenuItemClickListener {
                 gameInteractor.onFavoriteToggle(game, true)
                 true
             }
