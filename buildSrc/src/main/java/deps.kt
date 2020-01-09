5f7187cb2bedd31735a2bc2a4897d2fcaa398d2a
Filippo Scognamiglio <flscogna@gmail.com>
2020-01-09 22:04:11 +0100

    Update LibretroDroid version.

--- buildSrc/src/main/java/deps.kt
+++ buildSrc/src/main/java/deps.kt
@@ -22,7 +22,7 @@ object deps {
         const val navigation      = "2.1.0"
         const val rxbindings      = "3.0.0"
         const val lifecycle       = "2.1.0"
-        const val libretrodroid   = "6c8b628067"
+        const val libretrodroid   = "e03ebd4017"
     }
 
     object libs {
