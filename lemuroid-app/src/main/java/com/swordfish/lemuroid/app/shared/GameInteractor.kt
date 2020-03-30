f4cbad47364db9b1d3db99b4c5ba1802e9664e84
Filippo Scognamiglio <flscogna@gmail.com>
2020-03-30 21:55:18 +0200

    Add delay to ui update to avoid animation while opening a game.

--- lemuroid-app/src/main/java/com/swordfish/lemuroid/app/shared/GameInteractor.kt
+++ lemuroid-app/src/main/java/com/swordfish/lemuroid/app/shared/GameInteractor.kt
@@ -5,6 +5,8 @@ import com.swordfish.lemuroid.app.shared.game.GameLauncherActivity
 import com.swordfish.lemuroid.lib.library.db.RetrogradeDatabase
 import com.swordfish.lemuroid.lib.library.db.dao.updateAsync
 import com.swordfish.lemuroid.lib.library.db.entity.Game
+import io.reactivex.Completable
+import java.util.concurrent.TimeUnit
 
 class GameInteractor(
     private val context: Context,
@@ -13,15 +15,25 @@ class GameInteractor(
 ) {
     fun onGamePlay(game: Game) {
         GameLauncherActivity.launchGame(context, game, true, useLeanback)
-        retrogradeDb.gameDao().updateAsync(game.copy(lastPlayedAt = System.currentTimeMillis())).subscribe()
+        updateGamePlayedTimestamp(game)
     }
 
     fun onGameRestart(game: Game) {
         GameLauncherActivity.launchGame(context, game, false, useLeanback)
-        retrogradeDb.gameDao().updateAsync(game.copy(lastPlayedAt = System.currentTimeMillis())).subscribe()
+        updateGamePlayedTimestamp(game)
     }
 
     fun onFavoriteToggle(game: Game, isFavorite: Boolean) {
         retrogradeDb.gameDao().updateAsync(game.copy(isFavorite = isFavorite)).subscribe()
+    }
+
+    private fun updateGamePlayedTimestamp(game: Game) {
+        Completable.timer(DB_UPDATE_DELAY_SECONDS, TimeUnit.SECONDS)
+            .andThen(retrogradeDb.gameDao().updateAsync(game.copy(lastPlayedAt = System.currentTimeMillis())))
+            .subscribe()
+    }
+
+    companion object {
+        private const val DB_UPDATE_DELAY_SECONDS = 1L
     }
 }
