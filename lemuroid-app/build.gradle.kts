ccaf04af7c79c2ff188afe6ef3b5724365764f97
Filippo Scognamiglio <flscogna@gmail.com>
2020-04-27 22:22:16 +0200

    Bump versionCode and name.

--- lemuroid-app/build.gradle.kts
+++ lemuroid-app/build.gradle.kts
@@ -11,8 +11,8 @@ plugins {
 android {
     defaultConfig {
         applicationId = "com.swordfish.lemuroid"
-        versionCode = 21
-        versionName = "1.3.0-rc1"
+        versionCode = 22
+        versionName = "1.3.0"
     }
 
     signingConfigs {
