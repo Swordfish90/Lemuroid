0a2c325fc22c22dba16de2c6845b3db3c78334d3
Filippo Scognamiglio <flscogna@gmail.com>
2020-05-02 18:12:35 +0200

    Display a toast message on tvs when no gamepads are connected.

--- lemuroid-app/src/main/java/com/swordfish/lemuroid/app/tv/game/TVGameActivity.kt
+++ lemuroid-app/src/main/java/com/swordfish/lemuroid/app/tv/game/TVGameActivity.kt
@@ -1,9 +1,15 @@
 package com.swordfish.lemuroid.app.tv.game
 
+import android.os.Bundle
+import com.swordfish.lemuroid.R
 import com.swordfish.lemuroid.app.shared.game.BaseGameActivity
 import com.swordfish.lemuroid.app.tv.gamemenu.TVGameMenuActivity
 import com.swordfish.lemuroid.lib.library.GameSystem
+import com.swordfish.lemuroid.lib.util.subscribeBy
 import com.swordfish.libretrodroid.GLRetroView
+import com.uber.autodispose.android.lifecycle.scope
+import com.uber.autodispose.autoDispose
+import timber.log.Timber
 
 class TVGameActivity : BaseGameActivity() {
 
@@ -11,5 +17,15 @@ class TVGameActivity : BaseGameActivity() {
 
     override fun getShaderForSystem(useShader: Boolean, system: GameSystem): Int {
         return if (useShader) GLRetroView.SHADER_CRT else GLRetroView.SHADER_DEFAULT
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        gamePadManager
+            .getGamePadsObservable()
+            .filter { it.isEmpty() }
+            .autoDispose(scope())
+            .subscribeBy(Timber::e) { displayToast(R.string.tv_game_message_missing_gamepad) }
     }
 }
