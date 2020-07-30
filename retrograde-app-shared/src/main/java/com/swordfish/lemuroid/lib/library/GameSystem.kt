bfc875290763bcd125b2fd53a2596a1e8aa9a8d4
Filippo Scognamiglio <flscogna@gmail.com>
2020-07-31 01:05:05 +0200

    Add psx support for chd.

--- retrograde-app-shared/src/main/java/com/swordfish/lemuroid/lib/library/GameSystem.kt
+++ retrograde-app-shared/src/main/java/com/swordfish/lemuroid/lib/library/GameSystem.kt
@@ -225,7 +225,7 @@ data class GameSystem(
                         "sony0",
                         "pcsx_rearmed_libretro_android.so.zip",
                         uniqueExtensions = listOf(),
-                        supportedExtensions = listOf("iso", "pbp"),
+                        supportedExtensions = listOf("iso", "pbp", "chd"),
                         scanOptions = ScanOptions(
                             scanByFilename = false,
                             scanByUniqueExtension = false,
