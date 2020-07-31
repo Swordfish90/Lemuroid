f3ea181c04157b87281b8a68ba498d9614b51f60
Filippo Scognamiglio <flscogna@gmail.com>
2020-08-01 01:29:24 +0200

    Ignore invalid files such as cue without bin or m3u playlists without associated files.

--- retrograde-app-shared/src/main/java/com/swordfish/lemuroid/lib/library/StorageFilesMerger.kt
+++ retrograde-app-shared/src/main/java/com/swordfish/lemuroid/lib/library/StorageFilesMerger.kt
@@ -16,9 +16,83 @@ object StorageFilesMerger {
             .map { it to listOf<BaseStorageFile>() }
             .toMap().toMutableMap()
 
+        mergeBinCueFiles(allFiles)
+        removeInvalidBinCuePairs(allFiles)
+        mergeM3UPlaylists(allFiles, storageProvider)
+        removeInvalidM3UPlaylists(allFiles, storageProvider)
+
+        return allFiles.map { GroupedStorageFiles(it.key, it.value) }
+    }
+
+    private fun removeInvalidM3UPlaylists(
+        allFiles: MutableMap<BaseStorageFile, List<BaseStorageFile>>,
+        storageProvider: StorageProvider
+    ) {
         val toBeRemoved = mutableListOf<BaseStorageFile>()
 
         allFiles.keys
+            .asSequence()
+            .filter { it.extension == "m3u" }
+            .forEach { m3uFile ->
+                val m3uFiles: List<String> = runCatching {
+                    storageProvider.getInputStream(m3uFile.uri)?.readLines()
+                }.getOrNull() ?: listOf()
+
+                val filesNames = allFiles[m3uFile]?.map { it.name } ?: listOf()
+
+                if (!filesNames.containsAll(m3uFiles)) {
+                    toBeRemoved.add(m3uFile)
+                }
+            }
+
+        toBeRemoved.forEach { allFiles.remove(it) }
+    }
+
+    private fun mergeM3UPlaylists(
+        allFiles: MutableMap<BaseStorageFile, List<BaseStorageFile>>,
+        storageProvider: StorageProvider
+    ) {
+        val toBeRemoved = mutableListOf<BaseStorageFile>()
+
+        allFiles.keys
+            .asSequence()
+            .filter { it.extension == "m3u" }
+            .forEach { m3uFile ->
+                val m3uFiles = runCatching {
+                    storageProvider.getInputStream(m3uFile.uri)?.readLines()
+                }.getOrNull() ?: listOf()
+
+                val dataFiles = allFiles.filter { it.key.name in m3uFiles }
+
+                allFiles[m3uFile] = allFiles[m3uFile]!! + dataFiles.flatMap {
+                    listOf(it.key) + it.value
+                }
+                toBeRemoved.addAll(dataFiles.keys)
+            }
+
+        toBeRemoved.forEach { allFiles.remove(it) }
+    }
+
+    private fun removeInvalidBinCuePairs(
+        allFiles: MutableMap<BaseStorageFile, List<BaseStorageFile>>
+    ) {
+        val toBeRemoved = mutableListOf<BaseStorageFile>()
+
+        allFiles.keys
+            .asSequence()
+            .filter { it.extension == "cue" }
+            .forEach { if (allFiles[it].isNullOrEmpty()) toBeRemoved.add(it) }
+
+        toBeRemoved.forEach { allFiles.remove(it) }
+    }
+
+    private fun mergeBinCueFiles(
+        allFiles: MutableMap<BaseStorageFile, List<BaseStorageFile>>
+    ) {
+        val toBeRemoved = mutableListOf<BaseStorageFile>()
+
+        allFiles.keys
+            .asSequence()
             .filter { it.extension == "cue" }
             .forEach { cueFile ->
                 val binFiles = allFiles
@@ -33,26 +107,7 @@ object StorageFilesMerger {
                 }
                 toBeRemoved.addAll(binFiles.keys)
             }
+
         toBeRemoved.forEach { allFiles.remove(it) }
-        toBeRemoved.clear()
-
-        allFiles.keys
-            .filter { it.extension == "m3u" }
-            .forEach { m3uFile ->
-                val m3uFiles = runCatching {
-                    storageProvider.getInputStream(m3uFile.uri)?.readLines()
-                }.getOrNull() ?: listOf()
-
-                val dataFiles = allFiles.filter { it.key.name in m3uFiles }
-
-                allFiles[m3uFile] = allFiles[m3uFile]!! + dataFiles.flatMap {
-                    listOf(it.key) + it.value
-                }
-                toBeRemoved.addAll(dataFiles.keys)
-            }
-        toBeRemoved.forEach { allFiles.remove(it) }
-        toBeRemoved.clear()
-
-        return allFiles.map { GroupedStorageFiles(it.key, it.value) }
     }
 }
