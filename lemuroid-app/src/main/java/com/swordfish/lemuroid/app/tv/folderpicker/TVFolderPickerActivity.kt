2ff4491d23ba1b64b13dc52109685481fbd3bdd2
Mr. Chip53 <freebrewnet@gmail.com>
2020-09-01 00:10:37 -0500

    Fix format errors

--- lemuroid-app/src/main/java/com/swordfish/lemuroid/app/tv/folderpicker/TVFolderPickerActivity.kt
+++ lemuroid-app/src/main/java/com/swordfish/lemuroid/app/tv/folderpicker/TVFolderPickerActivity.kt
@@ -60,7 +60,7 @@ class TVFolderPickerActivity : BaseTVActivity() {
         override fun onCreateActions(actions: MutableList<GuidedAction>, savedInstanceState: Bundle?) {
             super.onCreateActions(actions, savedInstanceState)
 
-            if(directory.parent != null)
+            if (directory.parent != null)
                 addAction(actions, ACTION_NAVIGATE, "Parent Directory", directory.parent)
 
             directory.listFiles()
