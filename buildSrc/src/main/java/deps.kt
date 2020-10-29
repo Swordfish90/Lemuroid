7f1e98546f6c98d4de33460eac0c6a36467240a6
Filippo Scognamiglio <flscogna@gmail.com>
2020-10-29 01:19:20 +0100

    Replace with libraries with tagged versions.

--- buildSrc/src/main/java/deps.kt
+++ buildSrc/src/main/java/deps.kt
@@ -22,8 +22,8 @@ object deps {
         const val rxbindings      = "3.0.0"
         const val lifecycle       = "2.1.0"
         const val leanback        = "1.1.0-alpha03"
-        const val libretrodroid   = "8ca938721"
-        const val radialgamepad   = "3aaafea0a"
+        const val libretrodroid   = "0.5.0"
+        const val radialgamepad   = "0.1.1"
     }
 
     object libs {
