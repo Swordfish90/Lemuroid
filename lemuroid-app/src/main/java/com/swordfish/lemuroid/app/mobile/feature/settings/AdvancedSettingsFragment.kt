691e0489a041fd4625cc90c087ad8477497cce02
Filippo Scognamiglio <flscogna@gmail.com>
2021-08-25 22:33:01 +0200

    Add missing files.

---
+++ lemuroid-app/src/main/java/com/swordfish/lemuroid/app/mobile/feature/settings/AdvancedSettingsFragment.kt
@@ -0,0 +1,60 @@
+package com.swordfish.lemuroid.app.mobile.feature.settings
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import com.swordfish.lemuroid.R
+import com.swordfish.lemuroid.app.shared.settings.SettingsInteractor
+import com.swordfish.lemuroid.lib.preferences.SharedPreferencesHelper
+import dagger.android.support.AndroidSupportInjection
+import javax.inject.Inject
+
+class AdvancedSettingsFragment : PreferenceFragmentCompat() {
+
+    @Inject lateinit var settingsInteractor: SettingsInteractor
+
+    override fun onAttach(context: Context) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        preferenceManager.preferenceDataStore =
+            SharedPreferencesHelper.getSharedPreferencesDataStore(requireContext())
+        setPreferencesFromResource(R.xml.mobile_settings_advanced, rootKey)
+    }
+
+    override fun onPreferenceTreeClick(preference: Preference?): Boolean {
+        when (preference?.key) {
+            getString(R.string.pref_key_reset_settings) -> handleResetSettings()
+        }
+        return super.onPreferenceTreeClick(preference)
+    }
+
+    private fun handleResetSettings() {
+        AlertDialog.Builder(requireContext())
+            .setTitle(R.string.reset_settings_warning_message_title)
+            .setMessage(R.string.reset_settings_warning_message_description)
+            .setPositiveButton(R.string.ok) { _, _ ->
+                settingsInteractor.resetAllSettings()
+                reloadPreferences()
+            }
+            .setNegativeButton(R.string.cancel) { _, _ -> }
+            .show()
+    }
+
+    private fun reloadPreferences() {
+        preferenceScreen = null
+        setPreferencesFromResource(R.xml.mobile_settings, null)
+    }
+
+    @dagger.Module
+    class Module
+}
