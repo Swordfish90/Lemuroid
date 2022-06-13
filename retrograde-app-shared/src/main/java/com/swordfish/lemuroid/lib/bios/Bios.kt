98db25430b069eddf8398f2c1bc39c071ee394cb
Filippo Scognamiglio <flscogna@gmail.com>
2022-06-13 21:16:11 +0200

    Update bios handling makes it easier to import nds firmwares.

--- retrograde-app-shared/src/main/java/com/swordfish/lemuroid/lib/bios/Bios.kt
+++ retrograde-app-shared/src/main/java/com/swordfish/lemuroid/lib/bios/Bios.kt
@@ -3,9 +3,12 @@ package com.swordfish.lemuroid.lib.bios
 import com.swordfish.lemuroid.lib.library.SystemID
 
 data class Bios(
-    val fileName: String,
-    val crc32: String,
+    val libretroFileName: String,
     val md5: String,
     val description: String,
-    val systemID: SystemID
-)
+    val systemID: SystemID,
+    val externalCRC32: String? = null,
+    val externalName: String? = null,
+) {
+    fun displayName() = externalName ?: libretroFileName
+}
