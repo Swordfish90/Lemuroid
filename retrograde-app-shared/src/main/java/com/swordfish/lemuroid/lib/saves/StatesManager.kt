d2ac2c9c240990c71dc3c10dbd297347464add92
Filippo Scognamiglio <flscogna@gmail.com>
2022-08-06 15:33:20 +0200

    Slight refactors to coroutines in StatesManager.

--- retrograde-app-shared/src/main/java/com/swordfish/lemuroid/lib/saves/StatesManager.kt
+++ retrograde-app-shared/src/main/java/com/swordfish/lemuroid/lib/saves/StatesManager.kt
@@ -14,36 +14,53 @@ import java.io.File
 // TODO Since states are core related we should not put them in the same folder. This break previous versions states
 // so I decided to manage a transition phase reading also the old directory. We should safely remove it in a few weeks.
 
-// TODO FILIPPO... Check that every file write is wrapped by Dispatchers.IO
 class StatesManager(private val directoriesManager: DirectoriesManager) {
 
-    suspend fun getSlotSave(game: Game, coreID: CoreID, index: Int): SaveState? {
+    suspend fun getSlotSave(
+        game: Game,
+        coreID: CoreID,
+        index: Int
+    ): SaveState? = withContext(Dispatchers.IO) {
         assert(index in 0 until MAX_STATES)
-        return getSaveState(getSlotSaveFileName(game, index), coreID.coreName)
+        getSaveState(getSlotSaveFileName(game, index), coreID.coreName)
     }
 
-    suspend fun setSlotSave(game: Game, saveState: SaveState, coreID: CoreID, index: Int) {
+    suspend fun setSlotSave(
+        game: Game,
+        saveState: SaveState,
+        coreID: CoreID,
+        index: Int
+    ) = withContext(Dispatchers.IO) {
         assert(index in 0 until MAX_STATES)
-        return setSaveState(getSlotSaveFileName(game, index), coreID.coreName, saveState)
+        setSaveState(getSlotSaveFileName(game, index), coreID.coreName, saveState)
     }
 
-    suspend fun getAutoSaveInfo(game: Game, coreID: CoreID): SaveInfo {
+    suspend fun getAutoSaveInfo(
+        game: Game,
+        coreID: CoreID
+    ): SaveInfo = withContext(Dispatchers.IO) {
         val autoSaveFile = getStateFile(getAutoSaveFileName(game), coreID.coreName)
         val autoSaveHasData = autoSaveFile.length() > 0
-        return SaveInfo(autoSaveFile.exists() && autoSaveHasData, autoSaveFile.lastModified())
+        SaveInfo(autoSaveFile.exists() && autoSaveHasData, autoSaveFile.lastModified())
     }
 
-    suspend fun getAutoSave(game: Game, coreID: CoreID) =
+    suspend fun getAutoSave(game: Game, coreID: CoreID) = withContext(Dispatchers.IO) {
         getSaveState(getAutoSaveFileName(game), coreID.coreName)
+    }
 
-    suspend fun setAutoSave(game: Game, coreID: CoreID, saveState: SaveState) =
+    suspend fun setAutoSave(
+        game: Game,
+        coreID: CoreID,
+        saveState: SaveState
+    ) = withContext(Dispatchers.IO) {
         setSaveState(getAutoSaveFileName(game), coreID.coreName, saveState)
+    }
 
     suspend fun getSavedSlotsInfo(
         game: Game,
         coreID: CoreID
-    ): List<SaveInfo> {
-        return (0 until MAX_STATES)
+    ): List<SaveInfo> = withContext(Dispatchers.IO) {
+        (0 until MAX_STATES)
             .map { getStateFileOrDeprecated(getSlotSaveFileName(game, it), coreID.coreName) }
             .map { SaveInfo(it.exists(), it.lastModified()) }
             .toList()
@@ -52,8 +69,8 @@ class StatesManager(private val directoriesManager: DirectoriesManager) {
     private suspend fun getSaveState(
         fileName: String,
         coreName: String
-    ): SaveState? = withContext(Dispatchers.IO) {
-        runCatchingWithRetry(FILE_ACCESS_RETRIES) {
+    ): SaveState? {
+        return runCatchingWithRetry(FILE_ACCESS_RETRIES) {
             val saveFile = getStateFileOrDeprecated(fileName, coreName)
             val metadataFile = getMetadataStateFile(fileName, coreName)
             if (saveFile.exists()) {
@@ -82,26 +99,26 @@ class StatesManager(private val directoriesManager: DirectoriesManager) {
         }
     }
 
-    private suspend fun writeMetadataToDisk(
+    private fun writeMetadataToDisk(
         fileName: String,
         coreName: String,
         metadata: SaveState.Metadata
-    ) = withContext(Dispatchers.IO) {
+    ) {
         val metadataFile = getMetadataStateFile(fileName, coreName)
         metadataFile.writeText(Json.encodeToString(SaveState.Metadata.serializer(), metadata))
     }
 
-    private suspend fun writeStateToDisk(
+    private fun writeStateToDisk(
         fileName: String,
         coreName: String,
         stateArray: ByteArray
-    ) = withContext(Dispatchers.IO) {
+    ) {
         val saveFile = getStateFile(fileName, coreName)
         saveFile.writeBytesCompressed(stateArray)
     }
 
     @Deprecated("Using this folder collisions might happen across different systems.")
-    private suspend fun getStateFileOrDeprecated(fileName: String, coreName: String): File {
+    private fun getStateFileOrDeprecated(fileName: String, coreName: String): File {
         val stateFile = getStateFile(fileName, coreName)
         val deprecatedStateFile = getDeprecatedStateFile(fileName)
         return if (stateFile.exists() || !deprecatedStateFile.exists()) {
@@ -111,25 +128,25 @@ class StatesManager(private val directoriesManager: DirectoriesManager) {
         }
     }
 
-    private suspend fun getStateFile(fileName: String, coreName: String): File = withContext(Dispatchers.IO) {
+    private fun getStateFile(fileName: String, coreName: String): File {
         val statesDirectories = File(directoriesManager.getStatesDirectory(), coreName)
         statesDirectories.mkdirs()
-        File(statesDirectories, fileName)
+        return File(statesDirectories, fileName)
     }
 
-    private suspend fun getMetadataStateFile(
+    private fun getMetadataStateFile(
         stateFileName: String,
         coreName: String
-    ) = withContext(Dispatchers.IO) {
+    ): File {
         val statesDirectories = File(directoriesManager.getStatesDirectory(), coreName)
         statesDirectories.mkdirs()
-        File(statesDirectories, "$stateFileName.metadata")
+        return File(statesDirectories, "$stateFileName.metadata")
     }
 
     @Deprecated("Using this folder collisions might happen across different systems.")
-    private suspend fun getDeprecatedStateFile(fileName: String): File = withContext(Dispatchers.IO) {
+    private fun getDeprecatedStateFile(fileName: String): File {
         val statesDirectories = directoriesManager.getInternalStatesDirectory()
-        File(statesDirectories, fileName)
+        return File(statesDirectories, fileName)
     }
 
     private fun getAutoSaveFileName(game: Game) = "${game.fileName}.state"
