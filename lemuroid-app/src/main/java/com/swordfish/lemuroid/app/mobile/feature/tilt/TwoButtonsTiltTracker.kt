1f95a159826471c60c9865a2df9d09e9d3d65e43
Filippo Scognamiglio <flscogna@gmail.com>
2022-08-14 18:38:33 +0200

    Improve two buttons tilting behaviour.

--- lemuroid-app/src/main/java/com/swordfish/lemuroid/app/mobile/feature/tilt/TwoButtonsTiltTracker.kt
+++ lemuroid-app/src/main/java/com/swordfish/lemuroid/app/mobile/feature/tilt/TwoButtonsTiltTracker.kt
@@ -5,9 +5,9 @@ import com.swordfish.radialgamepad.library.RadialGamePad
 class TwoButtonsTiltTracker(private val leftId: Int, private val rightId: Int) : TiltTracker {
 
     override fun updateTracking(xTilt: Float, yTilt: Float, pads: Sequence<RadialGamePad>) {
-        val bothPressed = yTilt < 0.1
         val leftPressed = xTilt < 0.25
         val rightPressed = xTilt > 0.75
+        val bothPressed = !leftPressed && !rightPressed && yTilt < 0.1
 
         pads.forEach {
             it.simulateKeyEvent(leftId, bothPressed || leftPressed)
