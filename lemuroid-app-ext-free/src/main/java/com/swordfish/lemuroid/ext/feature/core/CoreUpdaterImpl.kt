12e3b03a24f00e183b030020cec5d6bbdc27ec96
Filippo Scognamiglio <flscogna@gmail.com>
2023-03-10 18:45:00 +0100

    Add new CUT2 upscaling shader. (#622)

--- lemuroid-app-ext-free/src/main/java/com/swordfish/lemuroid/ext/feature/core/CoreUpdaterImpl.kt
+++ lemuroid-app-ext-free/src/main/java/com/swordfish/lemuroid/ext/feature/core/CoreUpdaterImpl.kt
@@ -45,7 +45,7 @@ class CoreUpdaterImpl(
 
     // This is the last tagged versions of cores.
     companion object {
-        private const val CORES_VERSION = "1.14"
+        private const val CORES_VERSION = "1.15"
     }
 
     private val baseUri = Uri.parse("https://github.com/Swordfish90/LemuroidCores/")
