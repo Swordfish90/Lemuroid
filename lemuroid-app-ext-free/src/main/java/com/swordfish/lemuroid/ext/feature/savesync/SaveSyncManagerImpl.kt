419b954f33d52770673ec8447e57aa4b0e02515a
Felix Nüsse <felix.nuesse@t-online.de>
2023-04-09 20:46:03 +0200

    copy non existent files

--- lemuroid-app-ext-free/src/main/java/com/swordfish/lemuroid/ext/feature/savesync/SaveSyncManagerImpl.kt
+++ lemuroid-app-ext-free/src/main/java/com/swordfish/lemuroid/ext/feature/savesync/SaveSyncManagerImpl.kt
@@ -2,6 +2,9 @@ package com.swordfish.lemuroid.ext.feature.savesync
 
 import android.app.Activity
 import android.content.Context
+import android.net.Uri
+import android.util.Log
+import androidx.documentfile.provider.DocumentFile
 import com.swordfish.lemuroid.common.kotlin.SharedPreferencesDelegates
 import com.swordfish.lemuroid.ext.R
 import com.swordfish.lemuroid.lib.library.CoreID
@@ -10,7 +13,12 @@ import com.swordfish.lemuroid.lib.savesync.SaveSyncManager
 import com.swordfish.lemuroid.lib.storage.DirectoriesManager
 import timber.log.Timber
 import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
 import java.text.SimpleDateFormat
+
 
 class SaveSyncManagerImpl(
     private val appContext: Context,
@@ -55,13 +63,79 @@ class SaveSyncManagerImpl(
         return appContext.getString(R.string.saf_last_sync_completed, dateString)
     }
 
-    // not needed
-    override fun getConfigInfo(): String = ""
+    override fun getConfigInfo(): String {
+        return storageUri
+    }
 
     override suspend fun sync(cores: Set<CoreID>) {
+
+        Log.e("TAG", "start")
         synchronized(SYNC_LOCK) {
             val saveSyncResult = runCatching {
-                performSaveSyncForCores(cores)
+                val safProviderUri = Uri.parse(storageUri)
+                val safDirectory = DocumentFile.fromTreeUri(appContext, safProviderUri)
+
+
+                if (safDirectory != null) {
+                    val safSaves = safDirectory.findFile("saves")
+                    val saveFiles = safSaves?.listFiles()
+
+                    if (saveFiles != null) {
+
+                        // copy from saf to internal
+
+                        for(saveFile in saveFiles) {
+                            if(!saveFile.name.isNullOrEmpty()) {
+
+                                val internalTarget = getInternalSaveFile(saveFile.name!!)
+                                if(internalTarget != null) {
+                                    Timber.tag("SAF to Internal")
+                                        .e("SAF: ${saveFile.name} - ${saveFile.lastModified()}; Internal: ${internalTarget.lastModified()}")
+                                    if (internalTarget.lastModified() < saveFile.lastModified()) {
+                                        Timber.tag("SAF to Internal").e("SAF is newer")
+                                    } else {
+                                        Timber.tag("SAF to Internal").e("Internal is newer")
+                                    }
+                                } else {
+                                    Log.e("SAF to Internal", "test: ${saveFile.name} does not exist in internal storage")
+                                    val internalDir = File(appContext.getExternalFilesDir(null), "saves");
+                                    val newTarget = File(internalDir, saveFile.name)
+                                    if (newTarget.createNewFile()) {
+                                        copyFromSafToInternal(saveFile, newTarget)
+                                    } else {
+                                        Timber.e("Could not create new file in internal storage")
+                                    }
+                                }
+
+                            }
+                        }
+
+                        // now copy from internal to saf
+
+                        val savesInternal = File(appContext.getExternalFilesDir(null), "saves")
+
+                        for(i in savesInternal.listFiles()){
+                            val safTarget = safSaves.findFile(i.name)
+                            if(safTarget != null) {
+                                Timber.tag("Internal to SAF")
+                                    .e("Internal: ${i.name} - ${i.lastModified()}; SAF: ${safTarget.lastModified()}")
+                                if (safTarget.lastModified() < i.lastModified()) {
+                                    Timber.tag("Internal to SAF").e("Internal is newer")
+                                } else {
+                                    Timber.tag("Internal to SAF").e("SAF is newer")
+                                }
+                            } else {
+                                val newTarget = safSaves.createFile("application/octet-stream", i.name)
+                                if (newTarget != null) {
+                                    copyFromInternalToSaf(newTarget, i)
+                                }
+                            }
+
+                        }
+
+                    }
+                }
+                lastSyncTimestamp = System.currentTimeMillis()
             }
 
             saveSyncResult.onFailure {
@@ -69,12 +143,57 @@ class SaveSyncManagerImpl(
             }
         }
     }
-
-
-    private fun performSaveSyncForCores(cores: Set<CoreID>) {
-
-        lastSyncTimestamp = System.currentTimeMillis()
+    private fun getInternalSaveFile(filename: String): File? {
+        val saves = File(appContext.getExternalFilesDir(null), "saves")
+        if(saves != null) {
+            for(i in saves.listFiles()!!){
+                if(i.name.equals(filename)) {
+                    return File(saves, i.name)
+                }
+            }
+        }
+        return null
     }
+
+    private fun copyFromSafToInternal(saf: DocumentFile, internal: File) {
+        val output: OutputStream = FileOutputStream(internal)
+        val input: InputStream? = appContext.contentResolver.openInputStream(saf.uri)
+        copyFile(input, output)
+
+    }
+
+    private fun copyFromInternalToSaf(saf: DocumentFile, internal: File) {
+        val output: OutputStream? = appContext.contentResolver.openOutputStream(saf.uri)
+        val input: InputStream = FileInputStream(internal)
+        copyFile(input, output)
+    }
+
+    private fun copyFile(input: InputStream?, output: OutputStream?) {
+        if(input == null) {
+            Timber.e("Could not read source file!")
+            return
+        }
+        if(output == null) {
+            Timber.e("Could not read target file!")
+            return
+        }
+
+        try {
+            try {
+                // Transfer bytes from in to out
+                val buf = ByteArray(1024)
+                var len: Int
+                while (input.read(buf).also { len = it } > 0) {
+                    output.write(buf, 0, len)
+                }
+            } finally {
+                output.close()
+            }
+        } finally {
+            input.close()
+        }
+    }
+
 
     override fun computeSavesSpace() = ""
 
@@ -85,4 +204,5 @@ class SaveSyncManagerImpl(
         const val GDRIVE_PROPERTY_LOCAL_PATH = "localPath"
         private val SYNC_LOCK = Object()
     }
+
 }
