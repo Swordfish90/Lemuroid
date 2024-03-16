e88e69281bbe03ee7803c44f593a3ccca1ed4fd7
Filippo Scognamiglio <flscogna@gmail.com>
2024-03-16 09:43:21 +0100

    Improve favorite toggle button.

--- lemuroid-app/src/main/java/com/swordfish/lemuroid/app/mobile/shared/compose/ui/FavoriteToggle.kt
+++ lemuroid-app/src/main/java/com/swordfish/lemuroid/app/mobile/shared/compose/ui/FavoriteToggle.kt
@@ -1,36 +1,36 @@
 package com.swordfish.lemuroid.app.mobile.shared.compose.ui
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.swordfish.lemuroid.R
 
 @Composable
 fun FavoriteToggle(isToggled: Boolean, onFavoriteToggle: (Boolean) -> Unit) {
-    val drawableId = if (isToggled) {
-        R.drawable.game_list_favorite_toggled
-    } else {
-        R.drawable.game_list_favorite_untoggled
-    }
-
-    Surface(
+    IconToggleButton(
+        checked = isToggled,
+        onCheckedChange = onFavoriteToggle,
         modifier = Modifier.fillMaxSize(),
-        onClick = { onFavoriteToggle(!isToggled) }
     ) {
-        Image(
-            modifier = Modifier
-                .width(40.dp)
-                .height(40.dp),
-            painter = painterResource(id = drawableId),
-            contentDescription = "",
-            contentScale = ContentScale.FillBounds
+        val image = if (isToggled) {
+            Icons.Default.Favorite
+        } else {
+            Icons.Default.FavoriteBorder
+        }
+        Icon(
+            image,
+            contentDescription = stringResource(R.string.favorites),
+            tint = MaterialTheme.colorScheme.onBackground,
+            modifier = Modifier.size(24.dp)
         )
     }
 }
