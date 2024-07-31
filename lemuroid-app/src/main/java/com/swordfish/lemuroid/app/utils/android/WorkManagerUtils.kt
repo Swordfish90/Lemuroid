3f9953da728ea8f05830dbb54763cb93515ced01
Filippo Scognamiglio <flscogna@gmail.com>
2024-07-31 23:12:22 +0200

    Add foreground service infos. Required for targetSdk 34.

---
+++ lemuroid-app/src/main/java/com/swordfish/lemuroid/app/utils/android/WorkManagerUtils.kt
@@ -0,0 +1,14 @@
+package com.swordfish.lemuroid.app.utils.android
+
+import android.app.Notification
+import android.content.pm.ServiceInfo
+import android.os.Build
+import androidx.work.ForegroundInfo
+
+fun createSyncForegroundInfo(notificationId: Int, notification: Notification): ForegroundInfo {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        ForegroundInfo(notificationId, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC)
+    } else {
+        ForegroundInfo(notificationId, notification)
+    }
+}
