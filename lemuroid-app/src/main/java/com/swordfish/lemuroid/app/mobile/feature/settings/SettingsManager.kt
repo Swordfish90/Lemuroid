fdb6956cf674badd2eab153ca513788f7455709d
Felix Nüsse <felix.nuesse@t-online.de>
2024-08-07 09:26:43 +0200

    reimplement dpad

--- lemuroid-app/src/main/java/com/swordfish/lemuroid/app/mobile/feature/settings/SettingsManager.kt
+++ lemuroid-app/src/main/java/com/swordfish/lemuroid/app/mobile/feature/settings/SettingsManager.kt
@@ -46,6 +46,8 @@ class SettingsManager(private val context: Context, sharedPreferences: Lazy<Shar
 
     suspend fun enableDeviceRumble() = booleanPreference(R.string.pref_key_enable_device_rumble, false)
 
+    suspend fun joinStickDpad() = booleanPreference(R.string.pref_key_join_stick_dpad, false)
+
     suspend fun cacheSizeBytes() =
         stringPreference(
             R.string.pref_key_max_cache_size,
