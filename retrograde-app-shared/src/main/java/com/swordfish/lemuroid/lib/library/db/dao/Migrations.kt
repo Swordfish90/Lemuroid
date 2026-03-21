c700de7206048feafab92a4bc6fb7e75e5799539
Verdial <99362544+wakeupver@users.noreply.github.com>
2026-03-21 18:02:46 +0700

    Add migration for patch_codes table creation

--- retrograde-app-shared/src/main/java/com/swordfish/lemuroid/lib/library/db/dao/Migrations.kt
+++ retrograde-app-shared/src/main/java/com/swordfish/lemuroid/lib/library/db/dao/Migrations.kt
@@ -45,4 +45,32 @@ object Migrations {
                 )
             }
         }
+
+    val VERSION_9_10: Migration =
+        object : Migration(9, 10) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL(
+                    """
+                    CREATE TABLE IF NOT EXISTS `patch_codes`(
+                        `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                        `gameId` INTEGER NOT NULL,
+                        `description` TEXT NOT NULL,
+                        `code` TEXT NOT NULL,
+                        `enabled` INTEGER NOT NULL DEFAULT 0,
+                        FOREIGN KEY(`gameId`) REFERENCES `games`(`id`) ON UPDATE NO ACTION ON DELETE CASCADE
+                    )
+                    """.trimIndent(),
+                )
+                database.execSQL(
+                    """
+                    CREATE UNIQUE INDEX IF NOT EXISTS `index_patch_codes_id` ON `patch_codes` (`id`)
+                    """.trimIndent(),
+                )
+                database.execSQL(
+                    """
+                    CREATE INDEX IF NOT EXISTS `index_patch_codes_gameId` ON `patch_codes` (`gameId`)
+                    """.trimIndent(),
+                )
+            }
+        }
 }
