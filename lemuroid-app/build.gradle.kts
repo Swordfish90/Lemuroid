5e26514180fcc0d997aee0767d0db5f94928db7f
Dim Chen <dim.chen@imversed.com>
2026-04-07 18:20:04 +0300

    Bumped up version to 1.9.5

--- lemuroid-app/build.gradle.kts
+++ lemuroid-app/build.gradle.kts
@@ -27,7 +27,7 @@ buildscript {
 android {
     val versionMajor = 1
     val versionMinor = 9
-    val versionPatch = 2
+    val versionPatch = 5
 
     namespace = "com.swordfish.lemuroid"
     buildFeatures.buildConfig = true
