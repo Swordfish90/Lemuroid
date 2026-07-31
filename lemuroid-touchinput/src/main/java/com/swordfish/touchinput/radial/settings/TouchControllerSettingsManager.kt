24257d5cb95d14487f9142797f8483c289d2aca6
Nikhil Kadiyan <nikhilcaddilac@gmail.com>
2026-07-31 21:42:11 +0530

    Add a per core toggle to make Input pad transparent

--- lemuroid-touchinput/src/main/java/com/swordfish/touchinput/radial/settings/TouchControllerSettingsManager.kt
+++ lemuroid-touchinput/src/main/java/com/swordfish/touchinput/radial/settings/TouchControllerSettingsManager.kt
@@ -29,6 +29,7 @@ class TouchControllerSettingsManager(private val sharedPreferences: SharedPrefer
         val rotation: Float = DEFAULT_ROTATION,
         val marginX: Float = DEFAULT_MARGIN_X,
         val marginY: Float = DEFAULT_MARGIN_Y,
+        val transparentBackground: Boolean = DEFAULT_TRANSPARENT_BACKGROUND,
     )
 
     private fun computeInsetsPaddings(
@@ -123,6 +124,7 @@ class TouchControllerSettingsManager(private val sharedPreferences: SharedPrefer
         const val DEFAULT_ROTATION = 0.0f
         const val DEFAULT_MARGIN_X = 0.0f
         const val DEFAULT_MARGIN_Y = 0.0f
+        const val DEFAULT_TRANSPARENT_BACKGROUND = false
 
         const val MAX_ROTATION = 45f
         const val MIN_SCALE = 0.75f
